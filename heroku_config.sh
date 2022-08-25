f63b044f89d3cf5a41bf6e42be7d6890990a6e82
Carlos-Castellanos <carloslopez@itmexicali.edu.mx>
2022-08-24 20:43:05 -0700

    test

--- heroku_config.sh
+++ heroku_config.sh
@@ -1,12 +1,5 @@
-
-heroku config:set SECRET_KEY="GB6v_6pD74cUD5rLLNLiq-4BxqNG1Bl0R9wR0-urLyw"
-
-
-
+heroku config:set SECRET_KEY=GB6v_6pD74cUD5rLLNLiq-4BxqNG1Bl0R9wR0-urLyw
 heroku config:set DEFAULT_FROM_EMAIL=calc70@outlook.com
 heroku config:set EMAIL_HOST=smtp.sendgrid.net
-
-
-
 heroku config:set EMAIL_HOST_USER=apikey
 heroku config:set EMAIL_HOST_PASSWORD=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
